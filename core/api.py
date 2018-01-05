229f82c283211c84d69c1a31dd705edb65d8f233
Dmitry Bespalov <bespalovd@brainbankers.com>
2018-01-05 18:13:19 +0300

    global reformat code

--- core/api.py
+++ core/api.py
@@ -1,189 +1,63 @@
 # -*- coding: utf-8 -*-
 
 from core.interface import print_line
-from core.interface import print_platforms
-from core.interface import print_projects
-from core.interface import print_components
-from core.interface import print_issues
-
-from core.webapi import RequestLogin
-from core.webapi import RequestCreatePlatform
-
-requestLogin = RequestLogin()
-requestCreatePlatform = RequestCreatePlatform()
-
+from core.webapi import WebAPI
 
 class API(object):
 
-    def route(self, action: dict) -> bool:
-        if isinstance(action, dict):
-            if self.check_action_structure(action):
-                # Action -> Save config
-                if action['action'] == Actions.SAVE_CONFIG:
-                    if 'team' not in action:
-                        print_line('Team is not defined. Use --team=your_team parameter.')
-                        return False
-                    if 'user' not in action:
-                        print_line('User is not defined. Use --user=your_user_name parameter.')
-                        return False
-                    if 'password' not in action:
-                        print_line('Password is not defined. Use --password=your_password parameter.')
-                        return False
-                    # Save config
-                # Action -> Load config
-                if action['action'] == Actions.LOAD_CONFIG:
-                    pass
-                # Action -> Create platform
-                if action['action'] == Actions.CREATE_PLATFORM:
-                    if 'platform' in action:
-                        if action['platform'] is not None:
-                            if action['platform'] != '':
-                                if 'description' not in action:
-                                    action['description'] = Messages.DEFAULT
-                                if action['description'] == '':
-                                    action['description'] = Messages.DEFAULT
-                                platforms = self.get_platforms()
-                                if action['platform'] not in platforms:
-                                    # Call Create Platform method
-                                    if self.action_create_platform(action=action):
-                                        print_line('Platform created successfully.')
-                                        return True
-                                    else:
-                                        print_line('Platform creation failed.')
-                                        return False
-                                else:
-                                    print_line('Platform already exists.')
-                                    return True
-                            else:
-                                print_line('Empty platform name.')
-                                return False
-                        else:
-                            print_line('Platform parameter is None.')
-                            return False
-                    else:
-                        print_line('Empty --platform parameter')
-                        return False
-                # Action -> Create project
-                if action['action'] == Actions.CREATE_PROJECT:
-                    if 'platform' in action:
-                        if action['platform'] is not None and action['platform'] != '':
-                            platforms = self.get_platforms()
-                            if action['platform'] in platforms:
-                                projects = self.get_projects(action['platform'])
-                                if action['platform'] not in projects:
-                                    # Call Create Project
-                                    if self.action_create_project(action=action):
-                                        print_line('Project created successfully.')
-                                        return True
-                                    else:
-                                        print_line('Project creation failed.')
-                                        return False
-                                else:
-                                    print_line('Project already exists.')
-                                    return True
-                            else:
-                                print_line('Platform does not exists.')
-                                return False
-                        else:
-                            print_line('Project parameter is empty or None.')
-                            return False
-                    else:
-                        print_line('Empty --project parameter.')
-                        return False
-                # Action Create Set
-                if action['action'] == Actions.CREATE_SET:
-                    if 'platform' in action:
-                        if action['platform'] is not None and action['platform'] != '':
-                            platforms = self.get_platforms()
-                            if action['platform'] in platforms:
-                                if 'project' in action:
-                                    projects = self.get_projects(action['platform'])
-                                    if action['project'] in projects:
-                                        if self.action_create_set(action=action):
-                                            print_line('Set created successfully.')
-                                            return True
-                                        else:
-                                            print_line('Set creation failed.')
-                                            return False
-                                    else:
-                                        print_line('Project does not specified for defined platform.')
-                                        return False
-                                else:
-                                    print_line('Project does defined.')
-                                    return False
-                            else:
-                                print_line('Platform does not exists.')
-                                return False
-                        else:
-                            print_line('Project parameter is empty or None.')
-                            return False
-                    else:
-                        print_line('Empty --platform parameter.')
-                        return False
-            else:
-                print_line('Unknown action type.')
-                return False
-        else:
-            print_line('Wrong command object format.')
+    def run_action(self, api_data: dict) -> bool:
+
+        if not self.check_action_structure(api_data=api_data):
             return False
 
-    # SAVE CONFIG
+        if not self.check_action_type(api_data=api_data):
+            return False
 
-    def save_config(self, action: dict) -> bool:
+        if api_data['action'] == Actions.SAVE_CONFIG:
+            return self.save_config(api_data=api_data)
+
+        if not self.load_config(api_data=api_data):
+            return False
+
+        if not self.action_login(api_data=api_data):
+            return False
+
+        if api_data['action'] == Actions.CREATE_PLATFORM:
+            return self.action_create_platform(api_data=api_data)
+
+        if api_data['action'] == Actions.CREATE_PROJECT:
+            return self.action_create_project(api_data=api_data)
+
+        if api_data['action'] == Actions.CREATE_SET:
+            return self.action_create_set(api_data=api_data)
+
+    def check_action_structure(self, api_data: dict) -> bool:
         pass
 
-    # LOAD CONFIG
-
-    def load_config(self, action: dict) -> dict:
+    def check_action_type(self, api_data: dict) -> bool:
         pass
 
-    # GET PLATFORMS
-
-    def get_platforms(self) -> list:
-        return []
-
-    # GET PROJECTS
-
-    def get_projects(self, platform: str) -> list:
-        return []
-
-    # CREATE_PLATFORM
-
-    def action_create_platform(self, action: dict) -> bool:
-        # Create Platform
+    def save_config(self, api_data: dict) -> bool:
         pass
 
-    # CREATE_PROJECT
-
-    def action_create_project(self, action: dict) -> bool:
+    def load_config(self, api_data: dict) -> bool:
         pass
 
-    # CREATE_SET
-
-    def action_create_set(self, action: dict) -> bool:
+    def action_login(self, api_data: dict) -> bool:
         pass
 
-    @staticmethod
-    def check_action_structure(action: dict) -> bool:
-        if 'command' in action and \
-                'target' in action and \
-                'format' in action and \
-                'method' in action and \
-                'file' in action:
-            return True
-        return False
+    def action_create_platform(self, api_data: dict) -> bool:
+        pass
+
+    def action_create_project(self, api_data: dict) -> bool:
+        pass
+
+    def action_create_set(self, api_data: dict) -> bool:
+        pass
 
 
 class Actions(object):
     SAVE_CONFIG = 'save_config'
-    LOAD_CONFIG = 'load_config'
     CREATE_PLATFORM = 'create_platform'
     CREATE_PROJECT = 'create_project'
     CREATE_SET = 'create_set'
-    SHOW_PLATFORMS = 'show_platforms'
-    SHOW_PROJECTS = 'show_projects'
-    SHOW_SET = 'show_set'
-    SHOW_ISSUES = 'show_issues'
-
-class Messages(object):
-    DEFAULT = 'default'
--- core/webapi.py
+++ core/webapi.py
@@ -1,28 +1,159 @@
 # -*- coding: utf-8 -*-
 
+import json
+import time
+import requests
+
 from core.interface import print_line
 
+class WebAPI(object):
 
-def singleton(cls):
-    instances = {}
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:45.0) Gecko/20100101 Firefox/45.0',
+        'token': ''}
+    login_url = "https://surepatch.com/api/auth/login"
+    organization_url = "https://surepatch.com/api/organization"
+    platform_url = "https://surepatch.com/api/platforms"
+    project_url = "https://surepatch.com/api/projects"
+    components_url = "https://surepatch.com/api/components"
+    login_payload = dict(
+        username=None,
+        password=None,
+        referalToken=None,
+        organization=None)
+    platform_payload = dict(
+        name='',
+        description='')
+    project_payload = dict(
+        platform_id='',
+        parent=None,
+        project_url=None,
+        project_id=None,
+        required_right='manage_projects',
+        name='',
+        denied_if_unpaid=True,
+        logo=None,
+        components=[])
+    components_payload = dict(
+        set_name='0.0.0',
+        components=[],
+        project_url=None)
 
-    def get_instance():
-        if cls not in instances:
-            instances[cls] = cls()
-        return instances[cls]
+    def login(self, api_data: dict) -> bool:
+        self.login_payload['username'] = api_data['user']
+        self.login_payload['password'] = api_data['password']
+        self.login_payload['referalToken'] = None
+        self.login_payload['organization'] = api_data['team']
 
-    return get_instance
+        try:
+            response = requests.post(
+                url=self.login_url,
+                headers=self.headers,
+                json=self.login_payload)
+            if response.status_code == 200:
+                try:
+                    login_response = json.loads(response.text)
+                    api_data['token'] = login_response['token']
+                    api_data['user_id'] = login_response['userID']
+                    api_data['org_id'] = login_response['orgID']
+                    api_data['organization'] = None
+                    print_line('Login success.')
+                    return True
+                except ValueError as json_value_exception:
+                    print_line(f'Response JSON parsing exception: {json_value_exception}')
+                    return False
+            print_line(f'Login failed. Status code: {response.status_code}')
+            return False
+        except requests.exceptions.HTTPError as http_exception:
+            print_line(f'HTTP Error: {http_exception}')
+            return False
+        except requests.exceptions.ConnectionError as connection_exception:
+            print_line(f'Connection error: {connection_exception}')
+            return False
+        except requests.exceptions.Timeout as timeout_exception:
+            print_line(f'Connection timeout: {timeout_exception}')
+            return False
+        except requests.exceptions.RequestException as request_exception:
+            print_line(f'Request exception: {request_exception}')
+            return False
 
-
-class RequestBase(object):
-    pass
-
-
-@singleton
-class RequestLogin(RequestBase):
-    pass
-
-
-@singleton
-class RequestCreatePlatform(RequestBase):
-    pass
+    def get_organization_parameters(self, api_data: dict) -> bool:
+        self.headers['token'] = api_data['token']
+        try:
+            response = requests.get(
+                url=self.organization_url,
+                headers=self.headers)
+            if response.status_code == 200:
+                try:
+                    organization_data = json.loads(response.text)[0]
+                    api_data['organization'] = dict(
+                        id=organization_data['_id'],
+                        name=organization_data['name'],
+                        token=organization_data['token'],
+                        ownerID=organization_data['owner_id']['_id'],
+                        username=organization_data['owner_id']['username'],
+                        first_name=organization_data['owner_id']['firstname'],
+                        last_name=organization_data['owner_id']['lastname'],
+                        token_expires=organization_data['owner_id']['tokenExpires'],
+                        password_expires=organization_data['owner_id']['passwordExpires'],
+                        first_sign_in=organization_data['owner_id']['firstSignin'],
+                        private_key=organization_data['owner_id']['privateKey'],
+                        connection_id=organization_data['owner_id']['connectionID'],
+                        version=organization_data['owner_id']['__v'],
+                        password=organization_data['owner_id']['password'],
+                        github=organization_data['owner_id']['github'],
+                        notifications=organization_data['owner_id']['notifications'],
+                        TFA=organization_data['owner_id']['TFA'],
+                        open_key=organization_data['owner_id']['openKey'],
+                        blocked_till=organization_data['owner_id']['blockedTill'],
+                        failed_attempts=organization_data['owner_id']['failedAttempts'],
+                        updated=organization_data['owner_id']['updated'],
+                        super=organization_data['owner_id']['super'],
+                        last_passwords=organization_data['owner_id']['lastPasswords'],
+                        url=organization_data['url'],
+                        stripe_id=organization_data['stripe_id'],
+                        team_plan_id=organization_data['team_plan_id'],
+                        all_projects=organization_data['allProjects'],
+                        platforms=[])
+                    for platform_data in organization_data['platforms']:
+                        platform = dict(
+                            id=platform_data['_id'],
+                            name=platform_data['name'],
+                            description=platform_data['description'],
+                            url=platform_data['url'],
+                            version=platform_data['__v'],
+                            projects=[])
+                        for project_data in platform_data['projects']:
+                            project = dict(
+                                id=project_data['_id'],
+                                name=project_data['name'],
+                                logo=project_data['logo'],
+                                organization_id=project_data['organization_id'],
+                                platform_id=project_data['platform_id'],
+                                current_component_set=project_data['current_component_set'],
+                                token=project_data['token'], url=project_data['url'],
+                                version=project_data['__v'], updated=project_data['updated'],
+                                created=project_data['created'], issues=project_data['issues'],
+                                component_set_history=project_data['component_set_history'],
+                                child_projects=project_data['child_projects'],
+                                parent_projects=project_data['parent_projects'])
+                            platform['projects'].append(project)
+                        api_data['organization']['platforms'].append(platform)
+                    return True
+                except ValueError as json_value_exception:
+                    print_line(f'Response JSON parsing exception: {json_value_exception}')
+                    return False
+            print_line(f'Get organization parameters failed. Status code: {response.status_code}')
+            return False
+        except requests.exceptions.HTTPError as http_exception:
+            print_line(f'HTTP Error: {http_exception}')
+            return False
+        except requests.exceptions.ConnectionError as connection_exception:
+            print_line(f'Connection error: {connection_exception}')
+            return False
+        except requests.exceptions.Timeout as timeout_exception:
+            print_line(f'Connection timeout: {timeout_exception}')
+            return False
+        except requests.exceptions.RequestException as request_exception:
+            print_line(f'Request exception: {request_exception}')
+            return False
