25337fd50aacbe92ee32d5ee87a2c9d3bd68784b
ws.bespalov@gmail.com <ws.bespalov@gmail.com>
2018-01-22 12:53:36 +0300

    create run bat

---
+++ run.bat
@@ -0,0 +1,4 @@
+echo "start surepatch"
+echo "save config"
+:: c:\Python36\python.exe surepatch.py --action=save_config --token=JHUtf75yvtjVu6vV23764JB86G2V7C624HDBJCHBSIDhbjhcbdcusvdj
+:: c:\Python36\python.exe surepatch.py --action=save_config --team=dima --user=ws.bespalov@gmail.com --password=Test123!
