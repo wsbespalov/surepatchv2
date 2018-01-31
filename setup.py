13296767383acc128216d116594257a0ff7684b7
Dmitry Bespalov <bespalovd@brainbankers.com>
2018-01-31 18:30:40 +0300

    make run files

---
+++ setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="SurePatch",
+    version="0.2.1",
+    author="Dmitry Bespalov",
+    author_email="ws.bespalov@gmail.com",
+    description=("CLI App - Console client for SurePatch project"),
+    packages=["surepatch", ],
+    license="BSD",
+    url="",
+    long_description=read("README.md"),
+    classifiers=[
+        "Development Status :: 5 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License"
+    ]
+)
